8b1fdf4a3a82fe4c1acfc77f4a01ccd666820066
SimmaVahini <rsimmavahini2005@gmail.com>
2026-03-17 12:14:52 +0530

    Create number_mirror.java

---
+++ number_mirror.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// Create a Scanner object to read input
+		Scanner sc = new Scanner(System.in);
+		
+		// Read the integer N
+		if(sc.hasNextInt()){
+		    int n = sc.nextInt();
+		    
+		    // Print the same integer N
+		    System.out.println(n);
+		}
+	}
+}
