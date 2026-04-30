93222a98c696c5a58d5581be024cb39a93f44f3d
SimmaVahini <rsimmavahini2005@gmail.com>
2026-04-30 19:50:58 +0530

    Add Main class to multiply input by 10

---
+++ count the notebook.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            System.out.println(N * 10);
+        }
+
+        sc.close();
+    }
+}
